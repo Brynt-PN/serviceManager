398b39bfbaeacabb790e7b61dcefb055113142bc
OscarArCa <O.arias76510989@gmail.com>
2025-03-02 23:56:39 -0500

    add rutes for pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,25 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import {mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp } from 'ionicons/icons';
+import {
+  mailOutline,
+  mailSharp,
+  paperPlaneOutline,
+  paperPlaneSharp,
+  heartOutline,
+  heartSharp,
+  archiveOutline,
+  archiveSharp,
+  trashOutline,
+  trashSharp,
+  warningOutline,
+  warningSharp,
+  bookmarkOutline,
+  bookmarkSharp,
+  hammerSharp,
+  newspaperSharp,
+  personCircleSharp
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
@@ -13,13 +31,13 @@ import {mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Service', url: '/service', icon: 'mail' },
-    { title: 'Afiliation', url: '/afiliation', icon: 'mail' },
-    { title: 'Request', url: '/request', icon: 'mail' },
-    { title: 'Profile', url: '/profile', icon: 'mail' },
+    { title: 'Service', url: '/service', icon: 'hammer' },
+    { title: 'Afiliation', url: '/afiliation', icon: 'heart' },
+    { title: 'Request', url: '/request', icon: 'newspaper' },
+    { title: 'Profile', url: '/profile', icon: 'person-circle' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {
-    addIcons({mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
+    addIcons({personCircleSharp, newspaperSharp, hammerSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
   }
 }
