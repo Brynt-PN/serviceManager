21014c3c6b0e6dedae4319c8da83265a3fa1187b
OscarArCa <O.arias76510989@gmail.com>
2025-03-05 10:58:13 -0500

    add rutes for pages

--- src/app/request/request.page.ts
+++ src/app/request/request.page.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonList,
+  IonItem,
+  IonLabel, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonImg, IonThumbnail
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-request',
   templateUrl: './request.page.html',
   styleUrls: ['./request.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonList, IonItem, IonLabel, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonImg, IonThumbnail, IonButton]
 })
-export class RequestPage implements OnInit {
+export class RequestPage {
+  arrSolicitudes : {imgUrl:String; Service: String; requester: String}[]=[
+    {imgUrl:"/assets/icon/Calidda.jpg",Service:"Gas Calidda", requester : "Oscar Jean Mario Arias Camasca"},
+    {imgUrl:"/assets/icon/netflix.jpg",Service:"Netflix", requester : "Jean Clairon Mario Arias Camasca"},
+    {imgUrl:"/assets/icon/onlyfans.jpg",Service:"OnlyFans", requester : "Brayant Palomino"}
+];
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
