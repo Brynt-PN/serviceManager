14ca0f22f080602bf0e192b42c1953902ababdf4
Oscar <o.arias76510989@gmail.com>
2025-04-06 12:37:06 -0500

    visata editar usuario completada

--- src/app/profile/edit-profile/edit-profile.page.ts
+++ src/app/profile/edit-profile/edit-profile.page.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader, IonCardTitle,
+  IonContent,
+  IonHeader, IonInput,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.page.html',
   styleUrls: ['./edit-profile.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonInput, IonButton]
 })
 export class EditProfilePage implements OnInit {
 
